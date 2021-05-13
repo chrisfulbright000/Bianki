c7c1d85a414d4b0e2f80e19372223a3bf991bfd5
REBBIANKI304 <83888344+REBBIANKI304@users.noreply.github.com>
2021-05-13 11:20:53 +0100

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,9 +1,8 @@
-ÿþ
-@echo off
-echo FreeRDP By NaufalCream!
+@echo off
+echo AdityaRDP By AdityaGANS!
 tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
-echo FreeRDP By NaufalCream!
+echo AdityaRDP By AdityaGANS!
 goto check
